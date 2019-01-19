66102c310c4044f60ca88b7b5b589bb619aecbb4
luwamamenay <luwamamenay@hotmail.com>
2019-01-19 12:47:51 -0800

    subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     }
 
     public static double subtract(double num1,double num2){
-        return num1 - num2;
+        double result =num1 - num2;
+        return result;
     }
 }
