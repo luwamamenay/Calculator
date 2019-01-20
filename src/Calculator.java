96693a995a90444d692e1cc8d33696bc75941e09
luwamamenay <luwamamenay@hotmail.com>
2019-01-19 16:59:37 -0800

    adding division

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,4 +15,8 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1 * num2 ;
     }
+
+    public static double divide(double num3, double num4) {
+        return num3 / num4;
+    }
 }
